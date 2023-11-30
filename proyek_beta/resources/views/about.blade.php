e2dbf522348d49749b44e0b68cb16f20ce7ce472
jennifer-r <jenniferrahardjo8@gmail.com>
2023-11-30 08:55:09 +0700

    about us

--- proyek_beta/resources/views/about.blade.php
+++ proyek_beta/resources/views/about.blade.php
@@ -1,8 +1,84 @@
 @extends('layouts.main')
 @section('content')
+    <style>
+        body {
+        font-family: 'Arial', sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f8f8f8;
+        color: #333;
+        }
+
+        header {
+        background-color: #333;
+        color: #fff;
+        text-align: center;
+        padding: 1em 0;
+        }
+
+        section {
+        max-width: 800px;
+        margin: 20px auto;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        }
+
+        h1, h2 {
+        color: #198754;
+        }
+
+        h3 {
+            color: #3AAD6B;
+        }
+
+        p {
+        line-height: 1.6;
+        font-size: 20px;
+        }
+
+        ul {
+        list-style-type: none;
+        padding: 0;
+        font-size: 20px;
+        }
+
+        li {
+        margin-bottom: 10px;
+        font-size: 20px;
+        }
+
+        img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 8px;
+        margin-top: 10px;
+        }
+    </style>
     <div class="container" style="min-height: 650px; padding-top:100px;">
-        <h2>About Us</h2>
-        {{-- isi about us masih perlu diganti --}}
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis possimus voluptas corporis nesciunt quis ratione facilis, nulla id pariatur soluta iste! Explicabo minima aperiam qui accusamus harum doloribus voluptatum deleniti delectus aliquid eos quidem exercitationem unde fugiat nemo eaque, architecto incidunt molestias quo laborum tempore corporis? Enim neque vel molestias libero autem. Quisquam, repudiandae ratione eveniet aliquam, vitae sapiente dignissimos nam aperiam iste temporibus, quidem veniam? Dolore unde rem doloremque itaque autem necessitatibus ab ipsam temporibus quia? Harum corrupti est molestias velit, perspiciatis nesciunt doloribus. <br><br> Voluptatem distinctio aperiam aut similique facilis? Quos eaque iste odit quasi porro veniam nemo dicta quis accusantium consectetur velit quae ut, non dolorum corporis asperiores similique accusamus minus est facere omnis quo! Rerum quasi reiciendis blanditiis laborum saepe? Alias molestias, vero vel exercitationem suscipit corporis itaque explicabo quae illum reprehenderit facere rerum veniam repudiandae illo, quos blanditiis harum veritatis expedita. Libero eum ad nemo temporibus facere nam, consequuntur rerum maiores assumenda, esse aut suscipit quis eaque quidem iste deleniti non omnis quod reprehenderit asperiores possimus officiis. Unde doloremque, itaque vitae libero molestiae nesciunt ratione modi corrupti sit dolor animi, alias architecto aliquam rem eligendi eveniet quidem delectus porro ullam. <br><br> Assumenda minima atque recusandae enim repudiandae beatae quaerat voluptates, itaque vero vitae exercitationem voluptas magnam debitis amet aut, necessitatibus libero. Quos distinctio, corrupti consectetur incidunt perferendis facere assumenda vel culpa sunt ea ipsum, eius tempora. Aspernatur error sequi odio labore voluptate itaque cumque corporis perspiciatis quas laborum vel tempora adipisci sapiente ducimus, fuga distinctio. Quas, nam temporibus inventore voluptatibus, officia quis velit sint unde expedita ratione beatae fugiat quo fuga, a quod totam similique! Ratione, natus. Iusto explicabo perspiciatis reprehenderit tempora nesciunt! Inventore omnis sit blanditiis, in atque dolore ab minus hic molestias explicabo? Earum maiores deleniti perspiciatis fugiat. Doloribus consequuntur eos facere cumque quidem?</p>
+        <h1>About Us</h1><hr>
+        <img src="{{ asset('images/aboutUs/ecommerce.svg') }}" alt="ilustrasi misi" style="width: 500px; float: right; margin: 30px">
+        <br><h3>Selamat Datang di Ticket House!</h3>
+        <p>Destinasi terbaik Anda untuk menemukan dan mendapatkan tiket untuk berbagai acara mulai dari seminar, konser, pariwisata, lokakarya, hingga tempat-tempat menarik lainnya. Kami bangga menyajikan platform yang memungkinkan Anda merasakan momen-momen istimewa tanpa ribet.</p><br>
+
+        <h3>Misi Kami</h3>
+        <p>Misi kami adalah menyediakan akses mudah dan cepat ke berbagai acara yang Anda inginkan. Dengan kecanggihan teknologi, kami berkomitmen untuk menjadikan proses pembelian tiket lebih sederhana dan menyenangkan bagi Anda.</p><br>
+
+        <h3>Keunggulan Kami</h3>
+        <ul>
+        <li><strong>Kode Referral yang Menguntungkan:</strong> Di Ticket House, kami mengerti bahwa setiap pelanggan berharga. Oleh karena itu, kami memberikan kesempatan kepada Anda untuk mendapatkan diskon atau poin dengan berbagi kebahagiaan. Gunakan kode referral Anda dan nikmati penawaran istimewa yang menunggu.</li>
+        <li><strong>Fitur E-Commerce Dasar yang Lengkap:</strong> Berbelanja di Ticket House tidak hanya tentang memilih acara yang diinginkan, tetapi juga tentang pengalaman berbelanja yang lengkap. Nikmati fitur e-commerce dasar seperti metode pembayaran yang beragam, konfirmasi cepat, dan dukungan pelanggan yang responsif.</li>
+        <li><strong>Pemetaan Acara Lokal:</strong> Kami memahami pentingnya mendekati acara-acara yang berlangsung di sekitar Anda. Ticket House dilengkapi dengan fitur pemetaan yang memungkinkan Anda menemukan acara yang sesuai dengan lokasi Anda. Ini memudahkan Anda untuk terlibat dalam pengalaman lokal yang tak terlupakan.</li>
+        </ul><br>
+
+        <h3>Bagaimana Ticket House Bekerja</h3>
+        <ol>
+        <li><strong>Pencarian yang Mudah:</strong> Temukan acara yang sesuai dengan minat dan preferensi Anda dengan mudah melalui fitur pencarian yang canggih.</li>
+        <li><strong>Pembelian yang Aman dan Mudah:</strong> Pilih jumlah tiket yang diinginkan, lakukan pembayaran dengan aman melalui metode pembayaran pilihan Anda, dan terima konfirmasi instan.</li>
+        <li><strong>Manfaatkan Kode Referral:</strong> Bagikan kegembiraan dengan teman-teman Anda dengan menggunakan kode referral Anda dan nikmati diskon atau poin menarik.</li>
+        </ol><br>
+
+        <p>Ticket House adalah rumah bagi semua jenis acara, dari yang paling trendi hingga yang paling tradisional. Bergabunglah dengan komunitas kami dan rasakan pengalaman belanja tiket yang tak terlupakan. Terima kasih telah memilih Ticket House sebagai teman setia Anda dalam menciptakan kenangan berharga.</p>
     </div>
 @endsection
